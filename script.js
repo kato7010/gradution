c50619e37c6b934a4d6d1bd01ba8d6e13ef9c69d
kato7010 <kato.design7010@gmail.com>
2024-09-30 09:54:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 alert('このウェブサイトは職業訓練の卒業制作で作成したサイトです。制作実績は、授業の一環で制作したものであり、公式サイトとはなんら関係はありません。');
-
-from http.cookies import SimpleCookie
-
-cookie = SimpleCookie()
-cookie["session_id"] = "your_session_id_here"
-cookie["session_id"]["samesite"] = "Lax"
-cookie["session_id"]["httponly"] = True
-cookie["session_id"]["secure"] = True  # HTTPS経由でしか送信されないように設定
-
-print(cookie.output())
